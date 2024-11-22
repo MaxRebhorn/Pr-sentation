d0a16a355cd9f612a01adbbf57c763d1916cea81
Max Rebhorn <M.Rebhorn@queo-group.com>
2024-11-22 15:51:59 +0100

    Präsi Commit

--- main.py
+++ main.py
@@ -2,12 +2,30 @@ from pc_klasse import pc
 
 
 def main():
-
     mein_neuer_pc = pc("my-new-pc")
 
-    mein_neuer_pc.cpu_wechseln("ryzen 5 5500")
+    # Wechseln Sie den CPU zu Ryzen 5 5500U
+    mein_neuer_pc.cpu_wechseln("ryzen 5 5500U", "AMD", "4.0 GHz", "6", "AM4", 65)
 
-    print(pc)
+    # Weitere Bauteile hinzufügen - ACHTUNG, diese Werte sind nur Beispiele und können nicht korrekt sein
+
+    mein_neuer_pc.mainboard_wechseln("B450 TOMAHAWK MAX", "MSI", "AM4", "ATX", 4, "DDR4", 55)
+
+    mein_neuer_pc.set_netzteil("EVGA 600 W1", "EVGA", "600 W", "80 PLUS", "Ja", "ATX", 0)
+
+    mein_neuer_pc.ram_wechseln("Vengeance LPX", "Corsair", "16 GB", "DDR4", "3200 MHz", 2, 3.2)
+
+    mein_neuer_pc.grafik_karte_wechseln("RTX 2070 SUPER",
+                                        "GeForce", "NVIDIA Turing",
+                                        "8 GB", "1770 MHz", 215)
+
+    mein_neuer_pc.speicher_wechseln("SATA SSD", "Samsung", "1 TB", 2)
+
+    mein_neuer_pc.gehäuse_wechseln("H510", "NZXT", "ATX Mid Tower", "schwarz")
+
+    gesamt_stromverbrauch = mein_neuer_pc.strom_verbrauch_berechnen()
+
+    print(mein_neuer_pc,gesamt_stromverbrauch)
 
 
 
--- pc_klasse.py
+++ pc_klasse.py
@@ -1,55 +1,84 @@
-class pc():
+from prettytable import PrettyTable
+
+class pc:
     def __init__(self,name):
         self.name = name
         self.cpu = "empty"
-        self.grafik_karte   = "empty"
-        self.mainboard  = "empty"
-        self.ram  = "empty"
+        self.grafik_karte = "empty"
+        self.mainboard = "empty"
+        self.ram = "empty"
         self.netzteil  = "empty"
-        self.speicher  = "empty"
-        self.gehäuse  = "empty"
+        self.speicher = "empty"
+        self.gehäuse = "empty"
         self.teile = [self.cpu,self.grafik_karte,self.mainboard,self.ram,self.netzteil,self.speicher,self.gehäuse]
 
+    def __str__(self):
+        # Define a table
+        table = PrettyTable()
+
+        # Add columns
+        table.field_names = ["Component", "Details"]
+
+        # Iterate over the PC components
+        for key, value in self.__dict__.items():
+            # If a component has more details in form of a dictionary
+            if isinstance(value, dict):
+                table.add_row([key.capitalize(), ""])  # add a row for the component with no details
+                for subkey, subvalue in value.items():
+                    # add a row for each detail of the component
+                    table.add_row(["", f'{subkey.capitalize()}: {subvalue}'])
+            else:
+                table.add_row([key.capitalize(), value])  # just add a row with the component and its value
+
+        return "\n" + str(table)
     # Mainboard wechseln Funktion
-    def mainboard_wechseln(self, modell, sockel, form_faktor, ram_slots, ram_slot_typ, stromverbrauch):
-        if self.cpu and sockel != self.cpu["sockel"]:
+    def mainboard_wechseln(self, modell, marke, sockel, form_faktor, ram_slots, ram_slot_typ, stromverbrauch):
+        if isinstance(self.cpu, dict) and sockel != self.cpu["sockel"]:
             raise "Das neue Mainboard ist nicht kompatibel mit dem Sockel des CPUs."
-        if self.ram and (ram_slots < self.ram["anzahl_der_module"] or ram_slot_typ != self.ram["speichertyp"]):
+        if isinstance(self.ram, dict) and (
+                ram_slots < self.ram["anzahl_der_module"] or ram_slot_typ != self.ram["speichertyp"]):
             raise "Das neue Mainboard ist nicht kompatibel mit dem RAM."
         self.mainboard = {
             "modell": modell,
+            "marke": marke,
             "sockel": sockel,
             "form_faktor": form_faktor,
             "ram_slots": ram_slots,
             "ram_slot_typ": ram_slot_typ,
-            "stromverbrauch": stromverbrauch }
+            "stromverbrauch": stromverbrauch
+        }
 
-    # Netztteil wechseln Funktion
-    def set_netzteil(self, modell, watt, zertifizierung, modular, form_faktor):
+    # Netzteil wechseln Funktion
+    def set_netzteil(self, modell, marke, watt, zertifizierung, modular, form_faktor, stromverbrauch):
         self.netzteil = {
             "modell": modell,
+            "marke": marke,
             "watt": watt,
             "zertifizierung": zertifizierung,
             "modular": modular,
-            "form_faktor": form_faktor }
-
-    # CPU wechseln Funktion
-    def cpu_wechseln(self, modell, taktfrequenz, kerne, sockel, stromverbrauch):
-        if self.mainboard and self.mainboard.get("sockel") != sockel:
-            raise "Der neue CPU ist nicht kompatibel mit dem Sockel des Mainboards."
-        self.cpu = {
-            "modell": modell,
-            "taktfrequenz": taktfrequenz,
-            "kerne": kerne,
-            "sockel": sockel,
+            "form_faktor": form_faktor,
             "stromverbrauch": stromverbrauch }
 
+    # CPU wechseln Funktion
+    def cpu_wechseln(self, modell, marke, taktfrequenz, kerne, socket, stromverbrauch):
+        if isinstance(self.mainboard, dict) and socket != self.mainboard.get("sockel"):
+            raise "Der neue CPU ist nicht kompatibel mit dem Socket des Mainboards."
+        self.cpu = {
+            "modell": modell,
+            "marke": marke,
+            "taktfrequenz": taktfrequenz,
+            "kerne": kerne,
+            "sockel": socket,
+            "stromverbrauch": stromverbrauch
+        }
+
     # RAM wechseln Funktion
-    def ram_wechseln(self, modell, speichergröße, speichertyp, taktfrequenz, anzahl_der_module, stromverbrauch):
+    def ram_wechseln(self, modell, marke, speichergröße, speichertyp, taktfrequenz, anzahl_der_module, stromverbrauch):
         if self.mainboard and (anzahl_der_module > self.mainboard["ram_slots"] or speichertyp != self.mainboard["ram_slot_typ"]):
             raise "Der neue RAM ist nicht kompatibel mit dem Mainboard."
         self.ram = {
             "modell": modell,
+            "marke": marke,
             "speichergröße": speichergröße,
             "speichertyp": speichertyp,
             "taktfrequenz": taktfrequenz,
@@ -57,31 +86,36 @@ class pc():
             "stromverbrauch": stromverbrauch }
 
     # Grafikkarte wechseln Funktion
-    def grafik_karte_wechseln(self, modell, chipset, speicher, taktung, stromverbrauch):
+    def grafik_karte_wechseln(self, modell, marke, chipset, speicher, taktung, stromverbrauch):
         self.grafik_karte = {
             "modell": modell,
+            "marke": marke,
             "chipset": chipset,
             "speicher": speicher,
             "taktung": taktung,
             "stromverbrauch": stromverbrauch }
 
     # Speicher wechseln Funktion
-    def speicher_wechseln(self, typ, größe, stromverbrauch):
+    def speicher_wechseln(self, typ, marke, größe, stromverbrauch):
         self.speicher = {
             "typ": typ,
+            "marke": marke,
             "größe": größe,
             "stromverbrauch": stromverbrauch }
 
     # Gehäuse wechseln Funktion
-    def gehäuse_wechseln(self, modell, typ, farbe):
+    def gehäuse_wechseln(self, modell, marke, typ, farbe):
         self.gehäuse = {
-             "modell": modell,
+            "modell": modell,
+            "marke": marke,
             "typ": typ,
             "farbe": farbe }
 
     # Funktion zur Berechnung des Stromverbrauchs
     def strom_verbrauch_berechnen(self):
-        gesamt_stromverbrauch = None
+        gesamt_stromverbrauch = 0
         for pc_teil in self.teile:
-            gesamt_stromverbrauch += pc_teil["stromverbrauch"]
+            if pc_teil != "empty":
+                gesamt_stromverbrauch += pc_teil["stromverbrauch"]
         return gesamt_stromverbrauch
+
